2e425e214353314c66f84b7b218396ef47c840d7
Karam Deol <kdeol91@gmail.com>
2022-04-13 18:17:39 -0500

    find max/min

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,9 +6,6 @@ import numpy as np
 # Create dataframe
 df = pd.read_csv('budget_data.csv')
 
-# Print your data frame
-print(df.head())
-
 # List
 print(df["Date"])
 
@@ -18,20 +15,28 @@ print(len(df))
 # Print total of Profit/Losses with .sum
 print(df["Profit/Losses"].sum())
 
+# Set counter to 0
 difference= [0]
 
+# Start for loop
 for x in range(1, len(df)):
     difference.append(df["Profit/Losses"][x] - df["Profit/Losses"][x-1])
+
 df["differences"]=difference
+
+print(df["differences"][1:].mean())
 print(df.head())
 
+great_inc = (df["differences"].max())
+great_dec = (df["differences"].min())
 
 
 
+print("------------------")
 print("Financial Analysis")
 print("------------------")
 print("Total Months", (len(df)))
 print("Total", ((df["Profit/Losses"].sum())))
-print("Average Change", )
-print("Greatest Increase in Profits:", )
-print("Greatest Decrease in Profits:", )
+print("Average Change", (df["differences"][1:].mean()))
+print("Greatest Increase in Profits: $", [great_inc])
+print("Greatest Decrease in Profits: $", (great_dec))
